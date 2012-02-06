68977e68d35d3d50c00f11cb04cbe0d037cfd2ea
Ewout Vonk <dev@ewout.to>
2012-02-06 14:08:51 +0100

    further fixes

--- lib/edployscripts.rb
+++ lib/edployscripts.rb
@@ -4,6 +4,3 @@ require File.expand_path('../edployscripts/environment_scripts.rb', __FILE__)
 require File.expand_path('../edployscripts/file_handling.rb', __FILE__)
 require File.expand_path('../edployscripts/archive.rb', __FILE__)
 require File.expand_path('../edployscripts/git.rb', __FILE__)
-
-include Edploy::FileHandling
-include Edploy::Archive
